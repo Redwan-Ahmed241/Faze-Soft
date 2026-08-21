489f3da8df2e8a6fc0a2f0be75049429728855ad
Shah Redwan Ahmed <sahmed221241@bscse.uiu.ac.bd>
2026-08-21 13:55:55 +0600

    chore: update favicon assets, add site manifest, and improve social link security across all pages

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(function () {
     const options = {
         containers: ['#swupMain', '#swupMenu'],
         animateHistoryBrowsing: true,
-        linkSelector: 'a:not([data-no-swup])',
+        linkSelector: 'a:not([data-no-swup]):not([target="_blank"]):not([href^="http://"]):not([href^="https://"]):not([href^="mailto:"]):not([href^="tel:"]):not([href^="#"])',
         animationSelector: '[class="mil-main-transition"]'
     };
     const swup = new Swup(options);
